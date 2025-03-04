36fb45d615b2c9f588f4539d1577d80627143863
Bhaven <rathodbhavendilip@gmail.com>
2025-03-04 20:14:07 +0530

    delete and get all the products endpoint added

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,18 @@ app.get("/", (req, res) => {
   res.send("server is ready");
 });
 
+// get all products endpoint
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("Error in fetching all the products", error.message);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+});
+
+// create product endpoint
 app.post("/api/products", async (req, res) => {
   const product = req.body;
 
@@ -29,6 +41,19 @@ app.post("/api/products", async (req, res) => {
   } catch (error) {
     console.log("Server error", error.message);
     res.status(500).json({ success: false, message: "Server error" });
+  }
+});
+
+// delete product endpoint
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product deleted" });
+  } catch (error) {
+    console.log("Error in deleting product", error.message);
+    res.status(404).json({ success: false, message: "Product not found" });
   }
 });
 
